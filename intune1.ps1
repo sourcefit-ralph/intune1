4fd8d8f489325c855baa166ea39abeea7f3b46e9
sourcefit-ralph <ralph@sourcefit.net>
2024-07-03 21:01:21 +0800

    Update intune1.ps1

--- intune1.ps1
+++ intune1.ps1
@@ -1,4 +1,7 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
- Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
- Install-Script -Name Get-WindowsAutopilotInfo -Force
- Get-WindowsAutopilotInfo -Online -grouptag "PCNAMEHERE"
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
+Install-Script -Name Get-WindowsAutopilotInfo -Force
+
+$pcName = Read-Host -Prompt "Enter the PC name"
+Get-WindowsAutopilotInfo -Online -grouptag $pcName
+
