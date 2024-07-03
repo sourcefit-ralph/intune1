b804cb78d8fdf1747c0b9de481482b41ab6d28f0
sourcefit-ralph <ralph@sourcefit.net>
2024-07-03 21:50:39 +0800

    Update intune1.ps1

--- intune1.ps1
+++ intune1.ps1
@@ -1,3 +1,5 @@
+# Invoke-RestMethod -Uri https://link.sourcefit.info/intune1 | Invoke-Expression
+
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Set-ExecutionPolicy -Scope Process -ExecutionPolicy RemoteSigned
 Install-Script -Name Get-WindowsAutopilotInfo -Force
